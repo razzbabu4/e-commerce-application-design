850dc7e20e17e2a4f75e5559b3fe1ccb4c223c79
Tareq <razzbabut@gmail.com>
2026-03-01 01:56:59 +0600

    Complete product page desing and load all data in this page

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,17 @@ const loadProducts = async () => {
     const loader = document.getElementById("loader");
     try {
         loader?.classList.remove("hidden");
+        // loading all data
         const response = await fetch(url);
         const result = await response.json();
 
         // Load top three product based on rating
         if (document.getElementById("productCard")) {
             topProducts(result);
+        }
+        // Load all products
+        if (document.getElementById("allProductCard")) {
+            getAllProducts(result);
         }
         // Show a loading spinner when data is fetching
         if (loader) {
@@ -51,3 +56,22 @@ const topProducts = (products) => {
     });
 
 }
+
+const getAllProducts = ([...products]) => {
+    const container = document.getElementById("ourProductContainer");
+    const cardTemplate = document.getElementById("allProductCard");
+
+    products.forEach(product => {
+        const clone = cardTemplate.content.cloneNode(true);
+        clone.querySelector(".productImage").src = product.image;
+        clone.querySelector(".productRating").textContent = product.rating.rate;
+        clone.querySelector(".productRatingCount").textContent = `(${product.rating.count})`;
+        clone.querySelector(".productImage").alt = product.category;
+        clone.querySelector(".productCategory").textContent = product.category;
+        clone.querySelector(".productTitle").textContent = product.title;
+        clone.querySelector(".productPrice").textContent = "$" + product.price;
+        container.appendChild(clone);
+    });
+
+}
+
